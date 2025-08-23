6c3aacf7e821dfa61f3b8b01bdfa53ec33c852cc
Sudhanshu-shukl <shuklasudhanshu230405@gmail.com>
2025-08-24 02:14:46 +0530

    Added TCO to Fact

--- 05 Recursion/Factorial.c
+++ 05 Recursion/Factorial.c
@@ -1,14 +1,19 @@
 #include <stdio.h>
-int func(int n){
-    if (n == 1){
-        return 1;
+int func(int n,int ans){
+    if (n==0 || n == 1){
+        return ans;
     }
-    return n * func(n-1);
+    return func(n-1,ans*n);
     
 }
 int main(){
     int n = 5;
-    int n1 = func(n);
+    int n1 = func(n,1);
     printf("%d",n1);
     return 0;
 }
+
+
+// Ye hua ratta maarna -> 
+
+// TCO -> 
